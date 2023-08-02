a4403574e0cdac4823f0cba86a74ec9c76bb54db
gazchap <g.griffiths@gazchap.com>
2023-08-02 18:01:48 +0100

    Update from milestone branch and add banner to main

--- wordfence/cli/cli.py
+++ wordfence/cli/cli.py
@@ -2,11 +2,15 @@ import json
 import importlib
 import sys
 
+from wordfence.cli.banner import welcome_banner
 from .config import load_config
 
 
 def main():
     config = load_config()
+    if config.banner:
+        welcome_banner()
+
     subcommand_module = importlib.import_module(
             f'.{config.subcommand}.{config.subcommand}',
             package='wordfence.cli'
