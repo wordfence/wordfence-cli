18ac9db35280d4ec25b3258300612df6730c31a4
Jeremiah Sturgill <github@contact.jeremiahsturgill.com>
2023-08-05 20:18:17 -0700

    expanded setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
+from setuptools import setup, find_packages
 
-from distutils.core import setup
+from wordfence import __version__
 
-setup(name='Wordfence CLI',
-      version='0.0.1',
-      description='Command-line malware scanner powered by Wordfence',
-      py_modules=[],
-      )
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+setup(
+    name='wordfence',
+    version=__version__,
+    description='Command-line malware scanner powered by Wordfence',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/wordfence/wordfence-cli',
+    author='Wordfence',
+    author_email='devs@wordfence.com',
+    maintainer='Wordfence',
+    maintainer_email='devs@wordfence.com',
+    license='GPL 2.0',
+    license_files='LICENSE',
+    entry_points={
+        'console_scripts': [
+            'wordfence = wordfence.cli.cli:main',
+        ]
+    },
+    packages=find_packages(),
+    install_requires=['regex']
+)
