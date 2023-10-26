6f190c4f6abfe27206ccd8074db5e6794c2a005e
Brian Curran <brian@wordfence.com>
2023-10-26 11:05:34 -0400

    Add deb build

--- docker/build/entrypoint.sh
+++ docker/build/entrypoint.sh
@@ -3,75 +3,106 @@ set -e
 
 cd /root/wordfence-cli
 
-# build deb package
-
 ARCHITECTURE=$(dpkg --print-architecture)
-VERSION=$(python3 -c "from wordfence import version; print(version.__version__)")
-# CHANGELOG_VERSION=$(head -n 1 /root/wordfence-cli/debian/changelog | sed -n -E 's/wordfence \(([^)]+)\).*/\1/p')
+VERSION=$(python3 -c 'from wordfence import version; print(version.__version__)')
+GPG_USER='=Wordfence <opensource@wordfence.com>'
 
-export DEBFULLNAME=Wordfence
-export DEBEMAIL=opensource@wordfence.com
-echo "Generating changelog"
-dch \
-  --distribution unstable \
-  --check-dirname-level 0 \
-  --package wordfence \
-  --newversion "$VERSION" \
-  --create \
-  "$VERSION release. See https://github.com/wordfence/wordfence-cli for release notes."
-
-# install newer version of setuptools
-python3 -m pip install setuptools --force-reinstall
-
-# build the package
-dpkg-buildpackage -us -uc -b
-
-# build standalone executable
-
-# install requirements
+# install build requirements
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r requirements.txt --force-reinstall
 
-pyinstaller \
-  --name wordfence \
-  --onefile \
-  --hidden-import wordfence.cli.configure.configure \
-  --hidden-import wordfence.cli.configure.definition \
-  --hidden-import wordfence.cli.malwarescan.malwarescan \
-  --hidden-import wordfence.cli.malwarescan.definition \
-  --hidden-import wordfence.cli.vulnscan.vulnscan \
-  --hidden-import wordfence.cli.vulnscan.definition \
-  --hidden-import wordfence.cli.help.help \
-  --hidden-import wordfence.cli.help.definition \
-  --hidden-import wordfence.cli.version.version \
-  --hidden-import wordfence.cli.version.definition \
-  main.py
+if [ "$PACKAGE_TYPE" = 'deb' ] || [ "$PACKAGE_TYPE" = 'all' ]; then
 
-pushd /root/wordfence-cli/dist
+  # build deb package
 
-# compress the standalone executable, checksum and sign it, and copy both to the output directory
-STANDALONE_FILENAME="wordfence_${VERSION}_${ARCHITECTURE}_linux_exec"
-tar -czvf "${STANDALONE_FILENAME}.tar.gz" wordfence
-sha256sum "${STANDALONE_FILENAME}.tar.gz" > "${STANDALONE_FILENAME}.tar.gz.sha256"
-gpg \
-  --homedir "$CONTAINER_GPG_HOME_DIR" \
-  --detach-sign \
-  --armor \
-  --local-user '=Wordfence <opensource@wordfence.com>' \
-  "${STANDALONE_FILENAME}.tar.gz"
-gpg \
-  --homedir "$CONTAINER_GPG_HOME_DIR" \
-  --detach-sign \
-  --armor \
-  --local-user '=Wordfence <opensource@wordfence.com>' \
-  "${STANDALONE_FILENAME}.tar.gz.sha256"
-cp \
-  "${STANDALONE_FILENAME}.tar.gz" \
-  "${STANDALONE_FILENAME}.tar.gz.asc" \
-  "${STANDALONE_FILENAME}.tar.gz.sha256" \
-  "${STANDALONE_FILENAME}.tar.gz.sha256.asc" \
-  /root/output
+  export DEBFULLNAME='Wordfence'
+  export DEBEMAIL='opensource@wordfence.com'
+  echo 'Generating changelog'
+  dch \
+    --distribution unstable \
+    --check-dirname-level 0 \
+    --package wordfence \
+    --newversion "$VERSION" \
+    --create \
+    "${VERSION} release. See https://github.com/wordfence/wordfence-cli/releases/latest for release notes."
 
-popd
+  # build the package
+  dpkg-buildpackage -us -uc -b
 
-ls -lah "/root/output"
+  pushd ..
+
+  # sign and generate checksum
+  DEB_FILENAME="wordfence_${VERSION}_all"
+  sha256sum "${DEB_FILENAME}.deb" > "${DEB_FILENAME}.deb.sha256"
+  gpg \
+    --homedir "$CONTAINER_GPG_HOME_DIR" \
+    --detach-sign \
+    --armor \
+    --local-user "$GPG_USER" \
+    "${DEB_FILENAME}.deb"
+  gpg \
+    --homedir "$CONTAINER_GPG_HOME_DIR" \
+    --detach-sign \
+    --armor \
+    --local-user "$GPG_USER" \
+    "${DEB_FILENAME}.deb.sha256"
+  cp \
+    "${DEB_FILENAME}.deb" \
+    "${DEB_FILENAME}.deb.asc" \
+    "${DEB_FILENAME}.deb.sha256" \
+    "${DEB_FILENAME}.deb.sha256.asc" \
+    /root/output
+
+  popd
+
+fi
+
+if [ "$PACKAGE_TYPE" = 'standalone' ] || [ "$PACKAGE_TYPE" = 'all' ]; then
+
+  # build standalone executable
+
+  pyinstaller \
+    --name wordfence \
+    --onefile \
+    --hidden-import wordfence.cli.configure.configure \
+    --hidden-import wordfence.cli.configure.definition \
+    --hidden-import wordfence.cli.malwarescan.malwarescan \
+    --hidden-import wordfence.cli.malwarescan.definition \
+    --hidden-import wordfence.cli.vulnscan.vulnscan \
+    --hidden-import wordfence.cli.vulnscan.definition \
+    --hidden-import wordfence.cli.help.help \
+    --hidden-import wordfence.cli.help.definition \
+    --hidden-import wordfence.cli.version.version \
+    --hidden-import wordfence.cli.version.definition \
+    main.py
+
+  pushd /root/wordfence-cli/dist
+
+  # compress the standalone executable, checksum and sign it, and copy both to the output directory
+  STANDALONE_FILENAME="wordfence_${VERSION}_${ARCHITECTURE}_linux_exec"
+  tar -czvf "${STANDALONE_FILENAME}.tar.gz" wordfence
+  sha256sum "${STANDALONE_FILENAME}.tar.gz" > "${STANDALONE_FILENAME}.tar.gz.sha256"
+  gpg \
+    --homedir "$CONTAINER_GPG_HOME_DIR" \
+    --detach-sign \
+    --armor \
+    --local-user "$GPG_USER" \
+    "${STANDALONE_FILENAME}.tar.gz"
+  gpg \
+    --homedir "$CONTAINER_GPG_HOME_DIR" \
+    --detach-sign \
+    --armor \
+    --local-user "$GPG_USER" \
+    "${STANDALONE_FILENAME}.tar.gz.sha256"
+  cp \
+    "${STANDALONE_FILENAME}.tar.gz" \
+    "${STANDALONE_FILENAME}.tar.gz.asc" \
+    "${STANDALONE_FILENAME}.tar.gz.sha256" \
+    "${STANDALONE_FILENAME}.tar.gz.sha256.asc" \
+    /root/output
+
+  popd
+
+fi
+
+ls -lah /root/output
--- docker/build/host-build.sh
+++ docker/build/host-build.sh
@@ -3,35 +3,36 @@
 if [ -z ${1:+x} ]; then
     echo "You must provide the path to the project (if called from within the root project dir, pass in '.')"
     exit 1
-fi
-
-if [ ! -d "$1" ]; then
+elif [ ! -d "$1" ]; then
     echo "The first argument must point to the project directory"
+    exit 1
+elif [ -z ${2:+x} ]; then
+    echo "You must provide the architecture as the second argument"
+    exit 1
+elif [ "$2" != "amd64" ] && [ "$2" != "arm64" ]; then
+    echo "Invalid architecture (must be amd64 or arm64)"
+    exit 1
+elif [ -z ${3:+x} ]; then
+    echo "You must provide the package type as the third argument"
+    exit 1
+elif [ "$3" != "deb" ] && [ "$3" != "standalone" ] && [ "$3" != "all" ]; then
+    echo "Invalid package type (must be deb, standalone, or all)"
+    exit 1
 fi
 
 PROJECT_DIR=$(realpath "$1")
-
 echo "output path: $PROJECT_DIR/docker/build/volumes/output"
-ARCHITECTURES=("amd64" "arm64")
-
-function build_and_package() {
-    ARCHITECTURE="$1"
-    GPG_HOME_DIR=$(gpgconf --list-dirs homedir)
-    GPG_SOCKET=$(gpgconf --list-dirs agent-socket)
-    CONTAINER_GPG_HOME_DIR="/var/run/host_gpg_home_dir"
-    docker run \
-        -it \
-        --rm \
-        --name "wfcli-build-container-${ARCHITECTURE}" \
-        --platform "linux/${ARCHITECTURE}" \
-        -v "${PROJECT_DIR}/docker/build/volumes/output/:/root/output:rw" \
-        -v "${GPG_HOME_DIR}:${CONTAINER_GPG_HOME_DIR}:rw" \
-        -v "${GPG_SOCKET}:${CONTAINER_GPG_HOME_DIR}/S.gpg-agent:rw" \
-        -e "CONTAINER_GPG_HOME_DIR=${CONTAINER_GPG_HOME_DIR}" \
-        "wfcli-build-$ARCHITECTURE"
-}
-
-for arch in "${ARCHITECTURES[@]}"
-do
-    build_and_package "$arch"
-done
+ARCHITECTURE="$2"
+PACKAGE_TYPE="$3"
+GPG_HOME_DIR=$(gpgconf --list-dirs homedir)
+GPG_SOCKET=$(gpgconf --list-dirs agent-socket)
+CONTAINER_GPG_HOME_DIR="/var/run/host_gpg_home_dir"
+docker run \
+    --name "wfcli-build-container-${ARCHITECTURE}" \
+    --platform "linux/${ARCHITECTURE}" \
+    -v "${PROJECT_DIR}/docker/build/volumes/output/:/root/output:rw" \
+    -v "${GPG_HOME_DIR}:${CONTAINER_GPG_HOME_DIR}:rw" \
+    -v "${GPG_SOCKET}:${CONTAINER_GPG_HOME_DIR}/S.gpg-agent:rw" \
+    -e "CONTAINER_GPG_HOME_DIR=${CONTAINER_GPG_HOME_DIR}" \
+    -e "PACKAGE_TYPE=${PACKAGE_TYPE}" \
+    "wfcli-build-$ARCHITECTURE"
