c2da8a89f24e6ab137081f4392c56147772c0cfc
Brian Curran <brian@wordfence.com>
2024-01-09 13:42:25 -0500

    Rename specfile, put pcre Requires inside package section

--- docker/build/entrypoint.sh
+++ docker/build/entrypoint.sh
@@ -35,14 +35,14 @@ if [ "$PACKAGE_TYPE" = 'deb' ]; then
 
 fi
 
-if [ "$PACKAGE_TYPE" = 'rpm' ] || [ "$PACKAGE_TYPE" = 'all' ]; then
+if [ "$PACKAGE_TYPE" = 'rpm' ]; then
 
   # build RPM package
 
-  VERSION=$(python3.11 -c 'from wordfence import version; print(version.__version__)')
+  VERSION=$(python3 -c 'from wordfence import version; print(version.__version__)')
+  SPECFILE="wordfence.spec"
 
   export PATH="${PATH}:/usr/local/bin"
-  # pip3 install -r requirements.txt --force-reinstall
 
   # setup directories for rpmbuild
   rpmdev-setuptree
@@ -50,13 +50,15 @@ if [ "$PACKAGE_TYPE" = 'rpm' ] || [ "$PACKAGE_TYPE" = 'all' ]; then
   # create source archive for rpmbuild
   tar -C /root -czvf "/root/v${VERSION}.tar.gz" wordfence-cli
   cp "/root/v${VERSION}.tar.gz" /root/rpmbuild/SOURCES/
-  cp python-wordfence.spec /root/rpmbuild/SPECS/
+  cp "$SPECFILE" /root/rpmbuild/SPECS/
 
   # build RPM
-  rpmbuild -bb -D "wordfence_version ${VERSION}" /root/rpmbuild/SPECS/python-wordfence.spec
+  rpmbuild -bb \
+    -D "wordfence_version ${VERSION}" \
+    "/root/rpmbuild/SPECS/${SPECFILE}"
 
   # copy to output volume
-  pushd ../rpmbuild/RPMS/noarch/
+  pushd /root/rpmbuild/RPMS/noarch/
   RPM_FILENAME="python3.11-wordfence-${VERSION}-1.el9.noarch"
   cp "${RPM_FILENAME}.rpm" /root/output/wordfence-el9.rpm
 fi
