19aef094232ac752e4937274a3ba1d8fe3e7ab95
Alex Kenion <alexk@wordfence.com>
2026-04-10 15:43:58 -0400

    Improved handling of report encoding to prevent errors

--- wordfence/util/encoding.py
+++ wordfence/util/encoding.py
@@ -11,3 +11,7 @@ def str_to_bytes(value: Optional[str]) -> Optional[bytes]:
     if value is None:
         return None
     return value.encode('latin1', 'replace')
+
+
+def force_encoding(encoding: str, value: str) -> str:
+    return value.encode(encoding, errors="replace").decode(encoding)
